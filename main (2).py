606986bf21203f5cc8fbd979f5bd57f7df6d2695
Gowrishankar9595 <144442134+Gowrishankar9595@users.noreply.github.com>
2023-10-13 12:09:30 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,40 +1,24 @@
-'''
-Important a function called sort_students that takes a list of student objects as input and sports the
-list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object 
-has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function 
-with different input lists of students.
-'''
-
-class Student:
-
-  def __init__(self, name, roll_number, cgpa):
-    self.name = name
-    self.roll_number = roll_number
-    self.cgpa = cgpa
+"""
+Write a function called linear_search_product that takes the list of produproducts and target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
 
 
-def sort_students(student_list):
-  # sort the list of students in descending order of CGPA
-  sorted_students = sorted(student_list,
-                          key=lambda student: student.cgpa,
-                           reverse=True)
-  # Syntax - lambda arg:exp
-  return sorted_students
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+      
+  return indices
+   
 
 # Example usage:
-students = [
-  Student("Abilesh","A001", 7.6), 
-  Student("Alwin","A008", 7.7),
-  Student("Bharath","A014", 7.8),
-  Student("Kavitha","A124", 7.9),
-]
-
-sorted_students = sort_students(students)
-
-# Print the sorted list of students
-for student in sorted_students:
-  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
-                                                     student.roll_number,
-                                                     student.cgpa))
-  
+products = ["shoes","boot","loafer","shoes","sandal","shoes",]
+target ="shoes"
+target2 ='apple'
+result = linearSearchProduct(products, target)
+print(result)
--- main (3).py
+++ main (3).py
@@ -1,19 +1,40 @@
-#2.2 Implement a class called Player that represents a cricket player. The Player class should have a method called play() which prints "The player is playing cricket. Derive two classes, Batsman and Bowler, from the Player class. Override the play() method in each derived class to print "The batsman is batting" and "The bowler is bowling", respectively. Write a program to create objects of both the Batsman and Bowler classes and call the play() method for each object.
-# Define the Player class
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
-# Define the Batsman class, derived from Player
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
-# Define the Bowler class, derived from Player
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+'''
+Important a function called sort_students that takes a list of student objects as input and sports the
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object 
+has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function 
+with different input lists of students.
+'''
+
+class Student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                          key=lambda student: student.cgpa,
+                           reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+
+
+# Example usage:
+students = [
+  Student("Abilesh","A001", 7.6), 
+  Student("Alwin","A008", 7.7),
+  Student("Bharath","A014", 7.8),
+  Student("Kavitha","A124", 7.9),
+]
+
+sorted_students = sort_students(students)
+
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                     student.roll_number,
+                                                     student.cgpa))
+  
